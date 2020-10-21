98b153f5694871eb0a006ea2b38a1bfc6191d2d0
NickI <nicholas@ingegneris.com>
2020-10-21 19:14:04 +1100

    Third

--- lib/stripe_checkout_stub.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:snackproudv1/Product.dart';
-
-void redirectToCheckout(BuildContext context, List<Product> products) =>
-    throw UnsupportedError('neither mobile nor web');
